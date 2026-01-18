106ff176e1fc5ca1d47188d0c801797b0f0f179f
farooq <145667687+farucq@users.noreply.github.com>
2026-01-18 19:50:07 +0530

    Add 3D UNet model definition in model.py

---
+++ model.py
@@ -0,0 +1,13 @@
+#3D UNet (MONAI)
+from monai.networks.nets import UNet
+
+def get_model():
+    model = UNet(
+        spatial_dims=3,
+        in_channels=4,
+        out_channels=3,
+        channels=(32, 64, 128, 256),
+        strides=(2, 2, 2),
+        num_res_units=2
+    )
+    return model
